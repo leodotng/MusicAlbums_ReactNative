cc104501cb9bad7c4a6b3a0f807ababeb3a62916
Josh <leong.jo@gmail.com>
2017-12-02 01:23:13 -0700

    album titles render to screen

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
 
-
  class AlbumList extends Component {
      state = { albums: [] };
 
@@ -10,12 +9,18 @@ import axios from 'axios';
          axios.get('https://rallycoding.herokuapp.com/api/music_albums')
          .then(response => this.setState({ albums: response.data}));
      }
+
+renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+}
+
+
     render() {
         console.log(this.state);
-        
+
         return (
             <View>
-            <Text>Album List!!!</Text>
+            {this.renderAlbums()}
             </View>
         );
     }
