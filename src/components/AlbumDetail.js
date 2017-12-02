0eea17b7e5a8f4d041e9825f5b0da4b88bba16c0
Josh <leong.jo@gmail.com>
2017-12-02 14:16:04 -0700

    styles for albumdetail component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
     return (
         <Card>
-        <CardSection>
-            <Text>{props.album.title}</Text>
-        </CardSection>
+            <CardSection>
+                <View></View>
+                <View style={styles.headerContentStyle}>
+                    <Text>{props.album.title}</Text>
+                    <Text>{props.album.artist}</Text>
+                </View>
+            </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    }
 };
 
 export default AlbumDetail;
