54b8ab5dfb5f834921aa7b100fd73f51783a95e2
Josh <leong.jo@gmail.com>
2017-12-02 01:42:43 -0700

    added card component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,30 +1,34 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
+import React, {Component} from 'react';
+import {View} from 'react-native';
 import axios from 'axios';
-import AlbumDetail from './AlbumDetail'
+import AlbumDetail from './AlbumDetail';
 
- class AlbumList extends Component {
-     state = { albums: [] };
+class AlbumList extends Component {
+    state = {
+        albums: []
+    };
 
-     componentWillMount() {
-         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-         .then(response => this.setState({ albums: response.data}));
-     }
+    componentWillMount() {
+        axios
+            .get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({albums: response.data}));
+    }
 
-renderAlbums() {
-    return this.state.albums.map(album => <AlbumDetail key={album.title} album={album} />
-    );
-}
-
+    renderAlbums() {
+        return this
+            .state
+            .albums
+            .map(album => <AlbumDetail key={album.title} album={album}/>);
+    }
 
     render() {
         console.log(this.state);
 
         return (
             <View>
-            {this.renderAlbums()}
+                {this.renderAlbums()}
             </View>
         );
     }
- }
- export default AlbumList;
+}
+export default AlbumList;
