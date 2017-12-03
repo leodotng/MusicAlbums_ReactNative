353db1edb721ad5ba943e1943a42668ef7788d5f
Josh <leong.jo@gmail.com>
2017-12-02 18:33:26 -0700

    finished to vid 54

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import { Text, View, Image, Linking} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ({album}) => {
-    const {title, artist, thumbnail_image, image} = album;
-    const {
-        thumbnailStyle, 
-        headerContentStyle,
-        thumbnailContainerStyle,
-        headerTextStyle,
-        imageStyle
-    } = styles;
+    const { title, artist, thumbnail_image, image, url } = album;
+    const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
     return (
         <Card>
             <CardSection>
                 <View style={thumbnailContainerStyle}>
                     <Image
                         style={thumbnailStyle}
-                        source={{ uri: thumbnail_image }}
-                        />
+                        source={{
+                        uri: thumbnail_image
+                    }}/>
                 </View>
                 <View style={headerContentStyle}>
                     <Text style={headerTextStyle}>{title}</Text>
@@ -29,13 +24,17 @@ const AlbumDetail = ({album}) => {
             </CardSection>
 
             <CardSection>
-            <Image
-            style={imageStyle} 
-            source={{ uri: image }} />
+                <Image
+                    style={imageStyle}
+                    source={{
+                    uri: image
+                }}/>
             </CardSection>
-            
+
             <CardSection>
-            <Button />
+                <Button onPress={() => Linking.openURL(url)}>
+                Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
